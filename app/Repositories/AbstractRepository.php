d665d37f6d8fbb0d4a834a221b6630149016e07e
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-23 14:28:36 +0400

    Implement token and user repositories

--- app/Repositories/AbstractRepository.php
+++ app/Repositories/AbstractRepository.php
@@ -2,7 +2,28 @@
 
 namespace App\Repositories;
 
-class AbstractRepository
-{
+use Illuminate\Database\Eloquent\Model;
 
+abstract class AbstractRepository
+{
+    protected string $model;
+
+    public function __construct()
+    {
+    }
+
+    public function create(array $data): Model
+    {
+        return $this->model::create($data);
+    }
+
+    public function update(Model $model, array $data): bool
+    {
+        return $model->update($data);
+    }
+
+    public function destroy(Model $model): bool
+    {
+        return $model->delete();
+    }
 }
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,7 +2,55 @@
 
 namespace App\Repositories;
 
-class UserRepository
-{
+use App\Contracts\JWT\TokenServiceInterface;
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Lcobucci\JWT\UnencryptedToken;
 
+class UserRepository extends AbstractRepository
+{
+    protected string $model = User::class;
+
+    public function __construct(
+        protected TokenServiceInterface $service
+    )
+    {
+        parent::__construct();
+    }
+
+    public function create(array $data): Authenticatable
+    {
+        $user = DB::transaction(function () use ($data) {
+            /** @var User $user */
+            $user = parent::create($data);
+
+            $tokenPayloads = $this->service->data(sub: $user->id);
+
+            /** @var Collection<UnencryptedToken> $tokens */
+            $tokens = $tokenPayloads->map(fn ($payload) => $this->service->build($payload));
+
+            /** @var Collection<array> $tokenData */
+            $tokenData = $tokens->map(function(UnencryptedToken $token) use ($user) {
+                return [
+                    'id' => $token->claims()->get('jti'),
+                    'token' => $token,
+                    'tokenable_id' => $user->id,
+                    'tokenable_type' => get_class($user),
+                ];
+            });
+
+            $user->tokens()->createMany($tokenData);
+
+            $user->setAttribute(
+                'new_tokens',
+                $tokenData->map(fn ($token) => new Token($token))
+            );
+
+            return $user;
+        });
+
+        return $user;
+    }
 }
