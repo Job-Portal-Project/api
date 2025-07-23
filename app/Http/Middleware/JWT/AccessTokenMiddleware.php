7d8bac656e1fc595ca68ae098169e1622e999a1f
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-23 14:33:51 +0400

    Introduce middlewares

---
+++ app/Http/Middleware/JWT/AccessTokenMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware\JWT;
+
+use App\Enums\JWT\TokenType;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class AccessTokenMiddleware extends TokenAbstractMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure(Request): (Response)  $next
+     * @return Response
+     * @throws Throwable
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $this->start($request, TokenType::ACCESS);
+
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/JWT/TokenAbstractMiddleware.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware\JWT;
+
+use App\Enums\JWT\TokenType;
+use App\Models\JWT\Token;
+use App\Traits\TokenValidation;
+use Closure;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+abstract class TokenAbstractMiddleware
+{
+    use TokenValidation;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @return Response
+     * @throws Throwable
+     */
+    abstract public function handle(Request $request, Closure $next): Response;
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws Throwable
+     */
+    protected function start(Request $request, TokenType $type): void
+    {
+        $token = $this->getTokenForRequest($request);
+
+        throw_if(
+            !$record = Token::where('id', $token?->claims()->get('jti'))->first(),
+            AuthenticationException::class
+        );
+
+        throw_if(
+            !$record->tokenIs($type),
+            AuthorizationException::class
+        );
+
+        $this->validate($token, $record);
+    }
+}
