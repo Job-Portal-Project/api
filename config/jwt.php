ae3ed19d390043a133ef9c95efba572930a0a5da
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-23 13:06:45 +0400

    Initialize keys and config

---
+++ config/jwt.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'public_key_path' => realpath(env("JWT_PUBLIC_KEY", storage_path("jwt/public_key.pem"))),
+    'private_key_path' => realpath(env("JWT_PRIVATE_KEY", storage_path("jwt/private_key.pem"))),
+    'access' => [
+        'ttl' => 30, // Time to live for access tokens (in minutes)
+        'cbu' => 0,  // Can be used after (in minutes)
+    ],
+    'refresh' => [
+        'ttl' => 21600, // Time to live for refresh tokens (in minutes - 15 days)
+        'cbu' => 0,       // Can be used after (in minutes)
+    ]
+];
