fa3277729fd06d14c20ccdc4f94b569693422ac5
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-23 16:53:15 +0400

    Rename JWT blacklist migration to fix date order

---
+++ database/migrations/2025_07_24_090701_create_jwt_token_blacklist_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jwt_token_blacklist', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('jwt_token_id');
+            $table->timestamps();
+
+            $table->foreign('jwt_token_id')->references('id')->on('jwt_tokens');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jwt_token_blacklist');
+    }
+};
