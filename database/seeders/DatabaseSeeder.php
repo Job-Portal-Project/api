bc7e5eba5bdeb03cc67818e355c3e657610de793
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-08-05 21:47:49 +0400

    Revert "Feature: Industry and occupation endpoints (#16)" (#18)
    This reverts commit 3a73769bc27bf8fa6c01b1ce07aa7bff3fbd0b45.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            IndustrySeeder::class,
         ]);
     }
 }
